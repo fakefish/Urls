a321a5a92d8261321f862fa637563a8b61df0302
Fakefish <fakefish.wjy@gmail.com>
2012-10-28 20:53:14 +0800

    some note

--- urls.js
+++ urls.js
@@ -6,15 +6,16 @@
 **************/
 
 var urls = (function (arg, url) {
-	// 例子：url="www.example.com:9090"
+	// 例子：url="www.example.com:8080"
 	var _url = url || window.location.toString();
 	// 判断url是否含有http或https，没有就加上
   if(!(_url.substring(0, 7) === 'http://' || _url.substring(0, 8) === 'https://')) 
   	_url = 'http://' + _url;
-  // "http://www.example.com:9090"
+  // "http://www.example.com:8080"
   url = _url.split('/');
-  // ["http:", "", "www.example.com:9090"]
+  // ["http:", "", "www.example.com:8080"]
   var host = url[2].split(':');
-  // ["www.test.com", "9090"]
+  // ["www.test.com", "8080"]
   var _l = {protocol:url[0], hostname:host[0], port:(host[1]||'80'), pathname:'/' + url.slice(3, url.length).join('/').split('?')};
+  // {protocl:"http:",hostname:"www.example.com", port:"8080",pathname:'/'}
 })(arg, url);
